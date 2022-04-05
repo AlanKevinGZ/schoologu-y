a7d9d61fc4ee8b5b877a1fbc174ac8e3c6ef29d8
AlanKevinGZ <hades120@hotmail.com>
2022-04-05 16:54:43 -0500

    kevin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './acceso/login/login.component';
+import { RecuperarComponent } from './acceso/recuperar/recuperar.component';
+import { RegistroComponent } from './acceso/registro/registro.component';
+import { InicioComponent } from './homepage/inicio/inicio.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'',component:InicioComponent },
+  {path:'login',component: LoginComponent},
+  {path:'registro',component: RegistroComponent},
+  {path:'recovery',component: RecuperarComponent},
+  
+  { path: 'nomina', loadChildren:()=>import('./nomina/nomina.module').then(m=>m.NominaModule) },
+
+  { path: '**', pathMatch: 'full', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/homepage/inicio/inicio.component.ts
@@ -0,0 +1,34 @@
+import { Component, ElementRef, HostListener, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.component.html',
+  styleUrls: ['./inicio.component.scss']
+})
+export class InicioComponent implements OnInit {
+
+  @ViewChild('stickyMenu') menuElement: any;
+  sticky: boolean = false;
+  elementPosition: any;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+ ngAfterViewInit(){
+    this.elementPosition = this.menuElement.nativeElement.offsetTop;
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  handleScroll(){
+    const windowScroll = window.pageYOffset;
+    console.log(windowScroll);
+    
+    if(windowScroll > 777){
+      this.sticky = true;
+    } else {
+      this.sticky = false;
+    }
+  }
+
+}
